3aa1e718647e199d05518296666cc52ce535885b
なるみ <weaper@gmail.com>
2019-11-17 02:03:07 +0800

    Move segmentator to server.py

--- server.py
+++ server.py
@@ -3,13 +3,46 @@ from concurrent import futures
 
 import click
 import grpc
+import torch
+from torchvision import transforms
+from torchvision.models.segmentation import deeplabv3_resnet101
 
 import segmentation_pb2
 import segmentation_pb2_grpc
-from segmentator import Segmentator
 from utils import load_bytes_image
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class ResizeCenterCrop(transforms.Compose):
+    def __init__(self, size):
+        super(ResizeCenterCrop, self).__init__([
+            transforms.Resize(size),
+            transforms.CenterCrop(size),
+        ])
+
+
+class Segmentator(object):
+    def __init__(self, use_cuda=True):
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() and use_cuda else 'cpu')
+        self.model = deeplabv3_resnet101(pretrained=True)
+        self.model.to(self.device)
+
+        self.transform = transforms.Compose([
+            ResizeCenterCrop(480),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+        ])
+
+    @torch.no_grad()
+    def __call__(self, image):
+        self.model.eval()
+
+        x = self.transform(image).unsqueeze(dim=0).to(self.device)
+        out = self.model(x)['out']
+        return out.squeeze(dim=0).cpu().numpy()
 
 
 class SegmentationServicer(segmentation_pb2_grpc.SegmentationServicer):
